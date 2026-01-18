156305075a16c7b9f32e2a3092fa9fb4a40d41f6
Yonas Assefa <110129376+Yonas19@users.noreply.github.com>
2026-01-18 23:31:23 +0400

    Bubble sort

---
+++ bubbleSort_inp.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int bubbleSort (int arr[], int n){
+    for (int i = 0; i < n - 1; i ++){
+        for (int j = 0; j < n - i - 1; j++){
+            if (arr[j] > arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+}
+
+int main(){
+    int n;
+
+    printf("Enter ur array size ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++){
+        printf("Enter the elements");
+        scanf("%d", &arr[i]);
+    }
+
+    bubbleSort(arr, n);
+
+    printf("Sorted array \n");
+
+    for (int i = 0; i < n; i++){
+        printf("%d", arr[i]);
+    }
+    return 0;
+}
